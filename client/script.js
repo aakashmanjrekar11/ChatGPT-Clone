3c6eac56b09db696f0dedd5f5477e430afb69731
aakashmanjrekar11 <aakashmanjrekar@gmail.com>
2023-01-20 16:45:52 +0530

    added handleSubmit

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from ".assets/bot.svg";
+import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
 const form = document.querySelector("form");
@@ -7,15 +7,16 @@ const chatContainer = document.querySelector("chatContainer");
 let loadInterval;
 
 function loader(element) {
-  element.textContent = "";
+  element.textContent = ""; //initially text is empty
 
   loadInterval = setInterval(() => {
-    element.textContent += ".";
+    element.textContent += "."; //incrementing single dot '.'
 
     if (element.textContent === "....") {
+      //once it reaches 4 dots, clear the textContent (empty)
       element.textContent = "";
     }
-  }, 300);
+  }, 300); //delay of 300ms
 }
 
 function typeText(element, index) {
@@ -23,12 +24,13 @@ function typeText(element, index) {
 
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.innerHTML += text.charAt(index);
-      index++;
+      //if AI is still typing
+      element.innerHTML += text.charAt(index); //insert current character at index to HTML
+      index++; //increment index
     } else {
-      clearInterval(interval);
+      clearInterval(interval); //clear
     }
-  }, 20);
+  }, 20); //delay of 20ms
 }
 
 function generateUniqueId() {
@@ -57,3 +59,36 @@ function chatStripe(isAI, value, uniqueID) {
       </div>
     `;
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault(); //prevents browser from reloading (default)
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt")); // false = user typing
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueID = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueID); // true = AI typing. And " " empty because it will type as it loads (using loader function)
+
+  // as user types, keeps scrolling down to see the message
+  // puts new message in view.
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  // uniqueID for every single message
+  const messageDiv = document.getElementById(uniqueID);
+
+  loader(messageDiv);
+};
+
+// call handleSubmit
+// either when the submit button is clicked
+form.addEventListener("submit", handleSubmit);
+// or when the 'enter' key is pressed (enter keycode:13)
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
