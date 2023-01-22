67eff65d122feff6d046f7770cef94cfef68f262
aakashmanjrekar11 <aakashmanjrekar@gmail.com>
2023-01-23 02:15:18 +0530

    updated server url

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from the server -> bot's response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chatgpt-clone-ranf.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
